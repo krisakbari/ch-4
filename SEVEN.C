9ac4705afd55de80e7a9750d2cb12bad9e629a22
krisakbari <137288271+krisakbari@users.noreply.github.com>
2023-06-21 04:10:51 -0700

    Add files via upload

---
+++ SEVEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ float l=80.68,area;
+ float b=45.35;
+ area=l*b;
+ printf("area of l*b=%f",area);
+ float h=45.4;
+ area=1.5*l*b;
+  1/2*b*h=%f"		  333
---
+++ SIX.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+/* squer */
+{
+  float l=80.5,area;
+  clrscr();
+  area=l*l;
+  printf("area of squer l*lk=%f",area);
+
+
+  getch();
+}
